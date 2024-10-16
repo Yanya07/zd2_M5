bb2567fbd17295d7167717b855c24875c6890b4a
Yanya07 <yankabumoney@gmail.com>
2024-10-16 12:20:50 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.форматToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.жирныйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.курсивToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.подчеркнутьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.цветToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 396);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.форматToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(54, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+
+            // 
+            // форматToolStripMenuItem
+            // 
+            this.форматToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.жирныйToolStripMenuItem,
+            this.курсивToolStripMenuItem,
+            this.подчеркнутьToolStripMenuItem,
+            this.цветToolStripMenuItem});
+            this.форматToolStripMenuItem.Name = "форматToolStripMenuItem";
+            this.форматToolStripMenuItem.Size = new System.Drawing.Size(75, 24);
+            this.форматToolStripMenuItem.Text = "Формат";
+
+            // 
+            // жирныйToolStripMenuItem
+            // 
+            this.жирныйToolStripMenuItem.Name = "жирныйToolStripMenuItem";
+            this.жирныйToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.жирныйToolStripMenuItem.Text = "Жирный";
+            this.жирныйToolStripMenuItem.Click += new System.EventHandler(this.жирныйToolStripMenuItem_Click);
+
+            // 
+            // курсивToolStripMenuItem
+            // 
+            this.курсивToolStripMenuItem.Name = "курсивToolStripMenuItem";
+            this.курсивToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.курсивToolStripMenuItem.Text = "Курсив";
+            this.курсивToolStripMenuItem.Click += new System.EventHandler(this.курсивToolStripMenuItem_Click);
+
+            // 
+            // подчеркнутьToolStripMenuItem
+            // 
+            this.подчеркнутьToolStripMenuItem.Name = "подчеркнутьToolStripMenuItem";
+            this.подчеркнутьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.подчеркнутьToolStripMenuItem.Text = "Подчеркнуть";
+            this.подчеркнутьToolStripMenuItem.Click += new System.EventHandler(this.подчеркнутьToolStripMenuItem_Click);
+
+            // 
+            // цветToolStripMenuItem
+            // 
+            this.цветToolStripMenuItem.Name = "цветToolStripMenuItem";
+            this.цветToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.цветToolStripMenuItem.Text = "Цвет текста";
+            this.цветToolStripMenuItem.Click += new System.EventHandler(this.цветToolStripMenuItem_Click);
+
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 424);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(164, 20);
+            this.statusLabel.Text = "Количество символов: 0";
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Текстовый редактор";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.RichTextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem форматToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem жирныйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem курсивToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem подчеркнутьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem цветToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+         
+    }
+}
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void текстBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Обновление статуса с количеством символов
+            statusLabel.Text = "Количество символов: " + textBox1.Text.Length;
+        }
+
+        private void жирныйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Изменение стиля текста на жирный
+            if (textBox1.SelectionFont != null)
+            {
+                Font currentFont = textBox1.SelectionFont;
+                FontStyle newFontStyle = textBox1.SelectionFont.Style ^ FontStyle.Bold;
+                textBox1.SelectionFont = new Font(currentFont, newFontStyle);
+            }
+        }
+
+        private void курсивToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Изменение стиля текста на курсив
+            if (textBox1.SelectionFont != null)
+            {
+                Font currentFont = textBox1.SelectionFont;
+                FontStyle newFontStyle = textBox1.SelectionFont.Style ^ FontStyle.Italic;
+                textBox1.SelectionFont = new Font(currentFont, newFontStyle);
+            }
+        }
+
+        private void подчеркнутьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Изменение стиля текста на подчеркнутый
+            if (textBox1.SelectionFont != null)
+            {
+                Font currentFont = textBox1.SelectionFont;
+                FontStyle newFontStyle = textBox1.SelectionFont.Style ^ FontStyle.Underline;
+                textBox1.SelectionFont = new Font(currentFont, newFontStyle);
+            }
+        }
+
+        private void цветToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Выбор цвета текста
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.SelectionColor = colorDialog.Color;
+                }
+            }
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Открытие файла
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.LoadFile(openFileDialog.FileName, RichTextBoxStreamType.PlainText);
+                }
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Сохранение файла
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.PlainText);
+                }
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Обновление статуса с количеством символов
+            statusLabel.Text = "Количество символов: " + textBox1.Text.Length;
+        }
+    }
+
+}
